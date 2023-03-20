692682c035e6b9b4569cdb64428eb3f3f8810b08
Phyo Htet Arkar <phyohtetarkar@gmail.com>
2023-03-20 22:26:37 +0630

    Updates

---
+++ _next/static/RmE2CeV-L5y2crKo5KBZl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/414-fa0ca5093fe600e5.js","static/chunks/pages/index-b1407bf44e531e63.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/pr2GJ_lDW8dMcW7ggiyKN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/414-fa0ca5093fe600e5.js","static/chunks/pages/index-20c4c70c2bf3bfc2.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
